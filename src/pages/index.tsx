8cdd8ab2f727d90847470fd34762e15d7c9cbafb
Weslley Ferraz <weslleyhenrique800@gmail.com>
2023-01-01 19:37:21 -0400

    a

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,40 @@
+import Image from 'next/image';
 import { styled } from '../styles';
-
-const Button = styled('button', {
-  backgroundColor: '$green500',
-  borderRadius: 4,
-  padding: '4px 8px',
-
-  span: {
-    fontWeight: 'bold',
-  },
-
-  '&:hover': {
-    filter: 'brightness(0.8)',
-  },
-});
+import { HomeContainer, Product } from '../styles/pages/home';
+import ImageProduct1 from '../assets/1.png';
+import ImageProduct2 from '../assets/2.png';
+import ImageProduct3 from '../assets/3.png';
 
 export default function Home() {
   return (
     <div>
       <h1>Olá mundo</h1>
-      <Button>
-        Enviar <span>Teste</span>
-      </Button>
+      <HomeContainer>
+        <Product>
+          <Image src={ImageProduct1} alt="" />
+
+          <footer>
+            <strong>Camisera X</strong>
+            <strong>R$ 79,99</strong>
+          </footer>
+        </Product>
+        <Product>
+          <Image src={ImageProduct2} alt="" />
+
+          <footer>
+            <strong>Camisera Y</strong>
+            <strong>R$ 69,99</strong>
+          </footer>
+        </Product>
+        <Product>
+          <Image src={ImageProduct3} alt="" />
+
+          <footer>
+            <strong>Camisera Z</strong>
+            <strong>R$ 74,99</strong>
+          </footer>
+        </Product>
+      </HomeContainer>
     </div>
   );
 }
