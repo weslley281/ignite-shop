88ede00daf520334caec214d24dc1bad5f18d463
Weslley Ferraz <weslleyhenrique800@gmail.com>
2023-01-08 10:33:29 -0400

    a

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,27 @@
 import { useRouter } from 'next/router';
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from '../../styles/pages/product';
 
 export default function Product() {
   const { query } = useRouter();
 
-  return <h1>Product o id é {query.id}</h1>;
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,99</span>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
+          ipsum, nam reprehenderit voluptates eaque at, architecto aperiam et
+          voluptatum in officia quae quos quaerat commodi! Voluptate, rerum
+          eveniet. Necessitatibus, reprehenderit.
+        </p>
+        <button>Comprar Agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  );
 }
