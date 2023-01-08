55108b34c7871bd773119fb3c553ccb97aa93e63
Weslley Ferraz <weslleyhenrique800@gmail.com>
2023-01-08 09:40:53 -0400

    a

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,10 @@
 import { useState } from 'react';
 import Image from 'next/image';
 import { stripe } from '../lib/stripe';
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import { useKeenSlider } from 'keen-slider/react';
 
 import { HomeContainer, Product } from '../styles/pages/home';
-
-import camiseta1 from '../assets/1.png';
-import camiseta2 from '../assets/2.png';
-import camiseta3 from '../assets/3.png';
-import camiseta4 from '../assets/4.png';
 
 import 'keen-slider/keen-slider.min.css';
 import Stripe from 'stripe';
@@ -50,7 +45,7 @@ export default function Home({ products }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ['data.default_price'],
   });
@@ -62,7 +57,10 @@ export const getServerSideProps: GetServerSideProps = async () => {
       id: product.id,
       name: product.name,
       imageUrl: product.images[0],
-      price: price.unit_amount! / 100,
+      price: new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+      }).format(price.unit_amount! / 100),
     };
   });
 
@@ -70,5 +68,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       products,
     },
+    revalidate: 60 * 60 * 2,
   };
 };
